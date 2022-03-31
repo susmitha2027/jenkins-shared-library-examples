eb2c11d87aaacf179c5b441157838f76f5da0ea5
susmitha2027 <102210744+susmitha2027@users.noreply.github.com>
2022-03-31 14:41:20 +0530

    Update example3.groovy

--- vars/example3.groovy
+++ vars/example3.groovy
@@ -1,1 +1,15 @@
+#!/usr/bin/env groovy
 
+def call(String repoUrl) {
+  pipeline {
+    agent any 
+    stages {
+      stage("checkout") {
+        steps {
+          git branch: 'master',
+            url: "${repoUrl}"
+        }
+      }
+    }
+  }
+}
