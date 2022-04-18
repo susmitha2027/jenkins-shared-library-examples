7e5ea241a901ab9205c61d864b00c17c8bc0d4c9
susmitha2027 <102210744+susmitha2027@users.noreply.github.com>
2022-04-19 00:09:15 +0530

    Update example3.groovy

--- vars/example3.groovy
+++ vars/example3.groovy
@@ -7,7 +7,12 @@ def call(String repoUrl) {
   pipeline {
     agent any 
     stages {
-      
+      stage("checkout") {
+        steps {
+          git branch: 'master',
+            url: "${repoUrl}"
+        }
+      }
      
       stage('read') {
            steps {
